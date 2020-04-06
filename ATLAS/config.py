cba62c6b73315b18cdaf05656cf401ee525fe03c
wuxiaohua1011 <wuxiaohua1011@gmail.com>
2020-04-06 11:58:22 -0700

    root path issue resovled using appdirs package, thank god

--- ATLAS/config.py
+++ ATLAS/config.py
@@ -3,11 +3,15 @@ Default static configuration for all applications
 """
 from pathlib import Path
 
+from appdirs import *
+
+appname = "ATLAS"
+appauthor = "FHL VIVE"
+
 INSTALLATION_PATH = Path(__file__).parent
 DEFAULT_STYLE_SHEET_PATH = INSTALLATION_PATH / "model" / "layout" / "MaterialDark.qss"  # the default style sheet
 
-
-ROOT = Path('~/ATLAS')
+ROOT = Path(user_data_dir(appname=appname, appauthor=appauthor))
 DEFAULT_SEGMENTATION_FILE_PATH = ROOT / "data" / "segment.json"  # refer to the default segment json file
 DEFAULT_SCENE_FILE_PATH = ROOT / "data" / "scene.ply"  # refer to the default scene
 DEFAULT_DATA_LOCATION = ROOT / "data"  # refer to the default data folder location
